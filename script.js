7709878fa6c9e808ae26f5f4abdc93017b9ab03d
Nellu-AI <ravil.nell@gmail.com>
2021-01-28 17:53:29 +0300

    Add date update when open modal, change styles a little

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const mainApp = document.getElementById("main");
 const myModal = document.getElementById("modal");
 const closeModButton = document.getElementById("closeModBtn");
 
+// Input fields for new task
+const taskContent = document.getElementById("taskContent");
+const taskDate = document.getElementById("taskDate");
+const taskStatus = document.getElementById("taskStatus");
+
+let today = new Date().toISOString().substr(0, 10);
+
 window.addEventListener("load", pageLoaded);
 
 button.addEventListener("click", onClickButton);
@@ -21,8 +28,18 @@ function pageLoaded() {
 
 function onClickButton() {
   myModal.classList.add("show");
+  taskDate.value = today;
+
+  checkInputContent([taskContent, taskDate]);
 }
 
 function closeModal() {
+  event.preventDefault();
   myModal.classList.remove("show");
+}
+
+function checkInputContent(array) {
+  array.forEach((element) => {
+    console.log(element.value.length);
+  });
 }
